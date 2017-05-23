669b0973eaf3f96b131bce59943d74ed42ccabe9
Aaron <skiskiacm@gmail.com>
2017-05-23 15:07:14 -0700

    removed unnecessary files

--- client/my_angular/config.js
+++ client/my_angular/config.js
@@ -4,31 +4,15 @@ app.config(function($routeProvider){
   $routeProvider
     .when('/',{
       templateUrl: 'partials/home.html',
-      controller: 'BoardController'
+
     })
     .when('/projects',{
       templateUrl: 'partials/projects.html',
-      controller: 'LoginController'
+
     })
     .when('/resume',{
       templateUrl: 'partials/resume.html',
-      controller: 'BoardController'
-    })
-    .when('/contact',{
-      templateUrl: 'partials/contact.html',
-      controller: 'BoardController'
-    })
-    .when('/topic/:topic_id',{
-      templateUrl: 'partials/topic.html',
-      controller: 'Controller3'
-    })
-    .when('/user/:user_id',{
-      templateUrl: 'partials/user.html',
-      controller: 'Controller5'
-    })
-    .when('/chat',{
-      templateUrl: 'partials/partial_6.html',
-      controller: 'Controller6'
+
     })
     .otherwise({
       redirectTo: '/'
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,20 +1,5 @@
-var serverController = require('./../controllers/server_controller.js')
+
 
 module.exports = function(app){
-	app.post('/register', serverController.register);
-	app.post('/login', serverController.login);
-	app.get('/logout', serverController.logout);
-	app.get('/current', serverController.current);
-	app.get('/messages', serverController.getMessages);
-	app.get('/user/:user_id', serverController.getUser);
-	app.get('/user_messages/:id', serverController.getUserMessages);
-	app.post('/message', serverController.createMessage);
-	app.get('/topics', serverController.getTopics);
-	app.get('/user_topics/:id', serverController.getUserTopics);
-	app.get('/topic/:topic_id', serverController.getTopic);
-	app.get('/topic/messages/:topic_id', serverController.getTopicMessagesComments);
-	app.post('/topic', serverController.createTopic);
-	app.get('/user_comments/:id', serverController.getUserComments);
-	app.post('/comment/:id', serverController.createComment);
 
 }
