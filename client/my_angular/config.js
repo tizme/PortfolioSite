9a1e3ed3e8a86796d5f15356460a63391db21dd4
unknown <skiskiacm@gmail.com>
2017-08-01 22:26:30 -0700

    clean urls!

--- client/my_angular/config.js
+++ client/my_angular/config.js
@@ -3,7 +3,7 @@ var app = angular.module('app', ['ngRoute', 'ngMessages', 'ngAnimate']);
 
 
 
-app.config(['$routeProvider', 'locationProvider', function($routeProvider, $locationProvider){
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $routeProvider
     .when('/',{
       templateUrl: 'partials/home.html',
