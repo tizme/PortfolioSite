731e2d5f8a8331e92487aa6fe957c3e84565579b
unknown <skiskiacm@gmail.com>
2017-08-01 22:22:59 -0700

    clean urls!

--- client/my_angular/config.js
+++ client/my_angular/config.js
@@ -20,7 +20,17 @@ app.config(['$routeProvider', 'locationProvider', function($routeProvider, $loca
     .otherwise({
       redirectTo: '/'
     });
-    $locationProvider.html5Mode(true);
+    if(window.history && window.history.pushState){
+            //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+
+         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+
+         // if you don't wish to set base URL then use this
+         $locationProvider.html5Mode({
+                 enabled: true,
+                 requireBase: false
+          });
+        }
 }]);
 
 // app.run(["$rootScope","$location", function($rootScope, $location) {
